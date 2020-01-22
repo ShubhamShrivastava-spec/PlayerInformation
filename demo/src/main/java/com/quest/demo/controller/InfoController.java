55b2256bc5a8f4b7f562fa9fcb57a1a04f764af2
ShubhamShrivastava-spec <60138429+ShubhamShrivastava-spec@users.noreply.github.com>
2020-01-22 21:55:37 +0530

    added logger

--- demo/src/main/java/com/quest/demo/controller/InfoController.java
+++ demo/src/main/java/com/quest/demo/controller/InfoController.java
@@ -1,6 +1,8 @@
 package com.quest.demo.controller;
 
 import com.quest.demo.service.PlayerInfoService;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class InfoController {
-    @Autowired
+
+	private static final Logger logger = Logger.getLogger(InfoController.class);
+    
+	@Autowired
     private PlayerInfoService playerInfoService;
 
     @GetMapping(value = "/getInfo/{id}", produces = "applicaton/json")
     public ResponseEntity<String> getInfo(@PathVariable String id) {
+    	logger.info("Inside controller trying to get information for id :- "+id);
         return new ResponseEntity<>(playerInfoService.getInfoService(Integer.parseInt(id)), HttpStatus.OK);
     }
 }
